f9585f2279bf608b4f0f802a6444f4b2b9f78be3
Derek Willis <dwillis@gmail.com>
2020-03-28 18:11:25 -0400

    working scraper in Python 3

--- scrapers/crime/scrape.py
+++ scrapers/crime/scrape.py
@@ -1,13 +1,13 @@
 import csv
 import requests
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 
-url = 'http://www.showmeboone.com/sheriff/JailResidents/JailResidents.asp'
-response = requests.get(url)
+url = 'https://report.boonecountymo.org/mrcjava/servlet/SH01_MP.I00290s?max_rows=500'
+response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 html = response.content
 
-soup = BeautifulSoup(html)
-table = soup.find('table', attrs={'class': 'resultsTable'})
+soup = BeautifulSoup(html, 'html.parser')
+table = soup.find('tbody', attrs={'class': 'stripe'})
 
 list_of_rows = []
 for row in table.findAll('tr')[1:]:
@@ -17,7 +17,7 @@ for row in table.findAll('tr')[1:]:
         list_of_cells.append(text)
     list_of_rows.append(list_of_cells)
 
-outfile = open("./inmates.csv", "wb")
+outfile = open("./inmates.csv", "wt")
 writer = csv.writer(outfile)
-writer.writerow(["Last", "First", "Middle", "Gender", "Race", "Age", "City", "State"])
+writer.writerow(["Last", "First", "Middle", "Suffix", "Gender", "Race", "Age", "City", "State"])
 writer.writerows(list_of_rows)
